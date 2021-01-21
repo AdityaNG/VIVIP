39084b4b4eedd44854bd234d9c397debde4a999d
Aditya <adityang5@gmail.com>
2021-01-21 21:19:28 +0530

    Unit Tests

--- tests/test_libvivp.py
+++ tests/test_libvivp.py
@@ -2,6 +2,8 @@ import unittest
 from test import support
 import libvivp
 import os
+import git
+import shutil
 
 class MyTestCase1(unittest.TestCase):
 
@@ -17,13 +19,29 @@ class MyTestCase1(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_feature_one(self):
+    def test_feature_install_remove(self):
         # Test feature one.
         libvivp.install("tmps/", ["https://github.com/AdityaNG/BasicModules", "https://github.com/AdityaNG/RippleCarryAdder"])
+        libvivp.remove("tmps/", ["https://github.com/AdityaNG/RippleCarryAdder"])
+        pass
+    
+    def test_feature_update(self):
+        # Test feature two.
+        libvivp.update("tmps/")
         pass
 
-    def test_feature_two(self):
+    def test_feature_list(self):
         # Test feature two.
+        libvivp.list_vivp("tmps/")
+        pass
+
+    def test_feature_testbench(self):
+        # Test feature
+        if os.path.isdir("tmps/RippleCarryAdder"):
+            shutil.rmtree("tmps/RippleCarryAdder")
+        git.Git("tmps/").clone("https://github.com/AdityaNG/RippleCarryAdder")
+        libvivp.update("tmps/RippleCarryAdder")
+        libvivp.execute("tmps/RippleCarryAdder")
         pass
 
 if __name__ == '__main__':
